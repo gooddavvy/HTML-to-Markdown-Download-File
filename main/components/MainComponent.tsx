9bd343cadf74b378fef143f0e9d38506c315100c
David Akin <110428134+gooddavvy@users.noreply.github.com>
2023-02-27 20:54:55 +0000

    Added a README

--- main/components/MainComponent.tsx
+++ main/components/MainComponent.tsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import { DownloadComponent } from './';
 
 export default function MainComponent(): JSX.Element {
+  var [textareaContent, setTextareaContent] = useState("");
   
   return <div>
 {/*     <DownloadComponent fileUrl='/test.txt' linkDownload='test.txt'>Hello World!</DownloadComponent> */}
-    <textarea></textarea>
+    <textarea
+      value={textareaContent}
+      style={{
+        fontFamily: "monospace"
+      }}
+      onChange={
+        e => setTextareaContent(e.target.value as React.SetStateAction<
+                                typeof e.target.value
+                                | typeof textareaContent
+                                >
+                               )
+      }></textarea>
   </div>;
 }
