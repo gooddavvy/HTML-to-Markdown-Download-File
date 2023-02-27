d1add105438b88787cb7be606b91bbca226a3f0b
David Akin <110428134+gooddavvy@users.noreply.github.com>
2023-02-27 21:36:59 +0000

    remove node_modules

--- main/components/MainComponent.tsx
+++ main/components/MainComponent.tsx
@@ -3,10 +3,16 @@ import { DownloadComponent } from './';
 
 export default function MainComponent(): JSX.Element {
   var [textareaContent, setTextareaContent] = useState("");
+  var [invisibleComponent, setInvisibleComponent] = useState("");
 
+  var onSubmit: React.FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    (e.target as any).value = "hello world!"
+  };
+  
   return <div>
     {/*     <DownloadComponent fileUrl='/test.txt' linkDownload='test.txt'>Hello World!</DownloadComponent> */}
-    <form>
+    <form onSubmit={onSubmit}>
       <textarea
         value={textareaContent}
         style={{
