ddff3bd8d6dd46f2c4b00ba38ee11b54d9996377
David Akin <110428134+gooddavvy@users.noreply.github.com>
2023-02-27 21:12:50 +0000

    added a gitignore file

--- main/components/MainComponent.tsx
+++ main/components/MainComponent.tsx
@@ -3,20 +3,22 @@ import { DownloadComponent } from './';
 
 export default function MainComponent(): JSX.Element {
   var [textareaContent, setTextareaContent] = useState("");
-  
+
   return <div>
-{/*     <DownloadComponent fileUrl='/test.txt' linkDownload='test.txt'>Hello World!</DownloadComponent> */}
-    <textarea
-      value={textareaContent}
-      style={{
-        fontFamily: "monospace"
-      }}
-      onChange={
-        e => setTextareaContent(e.target.value as React.SetStateAction<
-                                typeof e.target.value
-                                | typeof textareaContent
-                                >
-                               )
-      }></textarea>
+    {/*     <DownloadComponent fileUrl='/test.txt' linkDownload='test.txt'>Hello World!</DownloadComponent> */}
+    <form>
+      <textarea
+        value={textareaContent}
+        style={{
+          fontFamily: "monospace"
+        }}
+        onChange={
+          e => setTextareaContent(e.target.value as React.SetStateAction<
+            typeof e.target.value
+            | typeof textareaContent
+          >
+          )
+        }></textarea>
+    </form>
   </div>;
 }
