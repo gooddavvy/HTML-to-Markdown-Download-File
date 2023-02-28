b01bca9ea3776a343d0edaae4edb8464efaac34a
David Akins (AwesomDev) <110428134+gooddavvy@users.noreply.github.com>
2023-02-28 19:29:07 +0000

    changed something you don't need to worry about

--- main/components/MainComponent.tsx
+++ main/components/MainComponent.tsx
@@ -7,16 +7,22 @@ export default function MainComponent(): JSX.Element {
   var [invisibleComponent, setInvisibleComponent] = useState(<div />);
 
   var createFile = async function(): Promise<void> {
-    var response = await fetch(`/api/createFile?fileName=file.md&fileContent=${textareaContent}`);
-    var data = await response.text();
-    console.log(data);
+    try {
+      var response = await fetch(`/api/createFile?fileName=file.md&fileContent=${textareaContent}`);
+      var data = await response.text();
+      console.log(data);
+    }
+    catch (err) {
+      console.error(err);
+      // throw err
+    };
   };
 
   var onSubmit: React.FormEventHandler<HTMLFormElement> = e => {
     e.preventDefault();
     createFile();
     setInvisibleComponent(<p>Please wait for a few seconds while we get ready to download your file...</p>)
-    setTimeout(() => {}, 5000)
+    setTimeout(() => { }, 5000)
     setInvisibleComponent(<DownloadComponent fileUrl='/file.md' linkDownload='file.md'>Hello World!</DownloadComponent>);
   };
 
@@ -25,6 +31,9 @@ export default function MainComponent(): JSX.Element {
     <form onSubmit={onSubmit}>
       <textarea
         value={textareaContent}
+        rows={50}
+
+        placeholder={`Your HTML goes here...`}
         style={{
           fontFamily: "monospace"
         }}
@@ -35,6 +44,11 @@ export default function MainComponent(): JSX.Element {
           >
           )
         }></textarea>
+{/*     line 35 below */}
+      
+  {/*         width={100} */}
+{/*         columns={1000} */}
+{/*         <br /> */}
       <button type="submit">Submit</button>
     </form>
     <div style={{ /*visibility: "hidden"*/ }}>{invisibleComponent}</div>
--- main/pages/api/createFile.ts
+++ main/pages/api/createFile.ts
@@ -1,3 +1,5 @@
+// pages/api/createFile.ts
+
 import fs from 'fs';
 import path from 'path';
 
@@ -5,12 +7,13 @@ export default function handler(req: any, res: any) {
   var { fileName, fileContent } = req.query;
   var filePath = path.join(process.cwd(), 'public', fileName);
 
-  fs.writeFile(filePath, fileContent, function (err) {
+  fs.writeFile(filePath, fileContent.toString(), function(err) {
     if (err) {
+      console.error(err);
       res.status(500).send('Error creating file');
-      throw err;
     } else {
       res.status(200).send(`File ${fileName} created successfully`);
+      console.log(fileContent)
     }
   });
 }
